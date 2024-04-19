7bd7885651444f1a2ea19e80212ab3ba2266153f
LakshayKhanagwal <klakshay889@gmail.com>
2024-04-19 14:41:34 +0530

    Form have Record Shown

--- pages/form/form.js
+++ pages/form/form.js
@@ -65,6 +65,159 @@ function save() {
     }
 }
 
+function show() {
+    var username = localStorage.getItem("u_name")
+    firebase.database().ref().child("form").child(username).once("value", function (form) {
+
+        var obj = form.val()
+        Arr = Object.keys(obj)
+
+        Arr.map(function (keys) {
+            Table(obj[keys].Name, obj[keys].Form, obj[keys].Form_fees, obj[keys].Our_fees, obj[keys].Payment_mode, obj[keys].Phone, obj[keys].Date)
+        })
+
+
+    })
+}
+function Table(name, form, form_fees, our_fees, payment_mode, phone, date) {
+
+    var card = document.createElement("div")
+    card.className = "col-lg-3 col-10 div p-0 rounded-3 overflow-hidden"
+
+    var name_div = document.createElement("div")
+    name_div.className = "col-12 bg-primary-subtle pt-2 text-muted rounded-top-3 align-content-center text-center"
+
+    var name_p = document.createElement("p")
+    name_p.className = "fs-5"
+    name_p.innerHTML = name
+    name_div.append(name_p)
+
+    card.append(name_div)
+
+    var table_div = document.createElement("div")
+
+    var table = document.createElement("table")
+    table.className = "table table-striped"
+
+    var tbody = document.createElement("tbody")
+
+
+    //row1
+
+    row1 = document.createElement("tr")
+
+    row1_cell1 = document.createElement("td")
+    row1_cell1.innerHTML = "Form Name:"
+    row1.append(row1_cell1)
+
+    row1_cell2 = document.createElement("td")
+    row1_cell2.innerHTML = form
+    row1.append(row1_cell2)
+
+    tbody.append(row1)
+
+    //row2
+
+    row2 = document.createElement("tr")
+    row2_cell1 = document.createElement("td")
+    row2_cell1.innerHTML = "Form Fees:"
+    row2.append(row2_cell1)
+
+    row2_cell2 = document.createElement("td")
+    row2_cell2.innerHTML = form_fees
+    row2.append(row2_cell2)
+
+    tbody.append(row2)
+
+//row3
+
+row3 = document.createElement("tr")
+    row3_cell1 = document.createElement("td")
+    row3_cell1.innerHTML = "Our Fees:"
+    row3.append(row3_cell1)
+
+    row3_cell2 = document.createElement("td")
+    row3_cell2.innerHTML = our_fees
+    row3.append(row3_cell2)
+
+    tbody.append(row3)
+
+    //row4
+
+    row4 = document.createElement("tr")
+    row4_cell1 = document.createElement("td")
+    row4_cell1.innerHTML = "Payment Mode:"
+    row4.append(row4_cell1)
+
+    row4_cell2 = document.createElement("td")
+    row4_cell2.innerHTML = payment_mode
+    row4.append(row4_cell2)
+
+    tbody.append(row4)
+
+    //row5
+
+    row5 = document.createElement("tr")
+    row5_cell1 = document.createElement("td")
+    row5_cell1.innerHTML = "Phone No.:"
+    row5.append(row5_cell1)
+
+    row5_cell2 = document.createElement("td")
+    row5_cell2.innerHTML = phone
+    row5.append(row5_cell2)
+
+    tbody.append(row5)
+
+    //row6
+
+    row6 = document.createElement("tr")
+    row6_cell1 = document.createElement("td")
+    row6_cell1.innerHTML = "Date:"
+    row6.append(row6_cell1)
+
+    row6_cell2 = document.createElement("td")
+    row6_cell2.innerHTML = date
+    row6.append(row6_cell2)
+
+    tbody.append(row6)
+
+    //row7
+
+    row7=document.createElement("tr")
+    row7_cell1_button=document.createElement("td")
+    row7_cell1_button.colSpan="2"
+    row7_cell1_button.className="text-end text-dark pe-5"
+
+    //edit button
+
+    var edit_button=document.createElement("button")
+    edit_button.className="btn btn-warning me-1"
+    edit_button.innerHTML="Edit"
+
+    row7_cell1_button.append(edit_button)
+    
+    //delete button
+
+    var delete_button=document.createElement("button")
+    delete_button.className="btn btn-danger"
+    delete_button.innerHTML="Delete"
+
+    row7_cell1_button.append(delete_button)
+
+    row7.append(row7_cell1_button)
+
+    tbody.append(row7)
+
+    table.append(tbody)
+
+    table_div.append(table)
+
+    card.append(table_div)
+
+    document.getElementById("record_cards").append(card)
+}
+
+
 // for prevent arrow key to increase number in ijnput field
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -78,22 +231,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function show(){
-    window.open('test.html')
-}
